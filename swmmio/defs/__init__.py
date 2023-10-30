25df5231507bedcf0082ad3c1c8f4803ad32552f
everett <everett.snieder@gmail.com>
2023-10-30 12:40:21 -0700

    added metric version of section headers

--- swmmio/defs/__init__.py
+++ swmmio/defs/__init__.py
@@ -4,7 +4,9 @@ import yaml
 from swmmio.defs.sectionheaders import normalize_inp_config
 
 _DEFS_PATH = os.path.abspath(os.path.dirname(__file__))
-_HEADERS_YAML = os.path.join(_DEFS_PATH, 'section_headers.yml')
+_HEADERS_YAML = os.path.join(_DEFS_PATH, 'section_headers_cms.yml')
+_DEFAULT_VALS = os.path.join(_DEFS_PATH, 'default_values.yml')
+
 _INP_SECTIONS_YAML = os.path.join(_DEFS_PATH, 'inp_sections.yml')
 
 with open(_INP_SECTIONS_YAML, 'r') as f:
@@ -12,6 +14,9 @@ with open(_INP_SECTIONS_YAML, 'r') as f:
 
 with open(_HEADERS_YAML, 'r') as f:
     HEADERS_YML = yaml.safe_load(f)
+
+with open(_DEFAULT_VALS, 'r') as f:
+    DEFAULT_VALS = yaml.safe_load(f)
 
 INP_OBJECTS = normalize_inp_config(_inp_sections_conf_raw['inp_file_objects'])
 RPT_OBJECTS = normalize_inp_config(HEADERS_YML['rpt_sections'])
